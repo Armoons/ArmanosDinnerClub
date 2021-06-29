ee7283b333ce2b8eaf68ff7967dfdc6e3c6d5971
Hujanja <arman11102@mail.ru>
2021-06-28 20:22:38 -0700

    Shadow Fix

--- ArmanosDinnerClub/ArmanosDinnerClub/Model/OrderedProductCell.swift
+++ ArmanosDinnerClub/ArmanosDinnerClub/Model/OrderedProductCell.swift
@@ -17,9 +17,9 @@ class OrderedProductCell: UICollectionViewCell {
         self.layer.cornerRadius = 12
         self.layer.backgroundColor = UIColor.white.cgColor
         self.layer.shadowColor = UIColor.gray.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 3)//CGSizeMake(0, 2.0);
-        self.layer.shadowRadius = 5.0
-        self.layer.shadowOpacity = 1.0
+        self.layer.shadowOffset = CGSize(width: 0, height: 8)//CGSizeMake(0, 2.0);
+        self.layer.shadowRadius = 10.0
+        self.layer.shadowOpacity = 0.08
         self.layer.masksToBounds = false
         self.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.contentView.layer.cornerRadius).cgPath
         
