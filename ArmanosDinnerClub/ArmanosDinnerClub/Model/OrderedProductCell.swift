884896b00d23007472ac33cdc8c352278ef317af
Hujanja <arman11102@mail.ru>
2021-06-28 20:18:25 -0700

    Collection View

--- ArmanosDinnerClub/ArmanosDinnerClub/Model/OrderedProductCell.swift
+++ ArmanosDinnerClub/ArmanosDinnerClub/Model/OrderedProductCell.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class OrderedProductCell: UICollectionViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.layer.cornerRadius = 12
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 3)//CGSizeMake(0, 2.0);
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOpacity = 1.0
+        self.layer.masksToBounds = false
+        self.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.contentView.layer.cornerRadius).cgPath
+        
+    }
+    
+    func setupCell(product: Product){
+        self.nameLabel.text = product.Name
+        self.priceLabel.text = "\(product.Price) рублей"
+        self.productImage.image = product.Image
     }
 
 }
